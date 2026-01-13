a1f36d0f4d3d8c83954ad73d80871e6e109df652
Paula <paula.rm@gmail.com>
2026-01-13 18:54:22 +0100

    fix another wrong path

--- games-resources/script.js
+++ games-resources/script.js
@@ -24,7 +24,7 @@ document.addEventListener('DOMContentLoaded', () => {
 	let games = [];
 
 	// Load games from data.json
-	fetch('data.json')
+	fetch('game-resources/data.json')
 		.then(response => response.json())
 		.then(data => {
 			games = data;
